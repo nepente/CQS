226ed20bcdc368049aa34d8b41704379cf464b35
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-08-09 09:54:15 -0300

    :+1: Inclusão teste e tratamento de handler not found para command

---
+++ Nepente.Cqs.Test/CommandDispatcherTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleInjector;
+
+namespace Nepente.Cqs.Test
+{
+    [TestClass]
+    public class CommandDispatcherTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(HandlerNotFoundException))]
+        public void HandlerNotFoundExceptionTest()
+        {
+            var container = new Container();
+            container.Register<ICommandDispatcher>(() => new CommandDispatcher(container));
+
+            var commandDispatcher = container.GetInstance<ICommandDispatcher>();
+
+            commandDispatcher.Dispatch<UserCommand>(new UserCommand());
+        }
+
+        
+        class UserCommand : ICommand { }
+    }
+}
--- Nepente.Cqs/HandlerNotFoundException.cs
+++ Nepente.Cqs/HandlerNotFoundException.cs
@@ -4,7 +4,7 @@ namespace Nepente.Cqs
 {
     public class HandlerNotFoundException : Exception
     {
-        private const string message = "Erro ao encontrar um handler para a query solicitada. Associação entre Query e QueryResult inválida ou não registrada.";
+        private const string message = "Erro ao encontrar um handler para a query solicitada. Handler associado a Query e QueryResult ou Command não registrada.";
 
         public HandlerNotFoundException() : base(message)
         {}
