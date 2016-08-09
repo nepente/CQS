7db005923e4f296a0b1e58d27bc55203f81f5e77
Bernardo Oliveira Bruning <bernardo.bruning@gmail.com>
2016-08-09 09:35:16 -0300

    :construction: Criação do teste para query dispatcher

---
+++ Nepente.Cqs.Test/QueryDispatcherTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleInjector;
+
+namespace Nepente.Cqs.Test
+{
+    [TestClass]
+    public class QueryDispatcherTest
+    {
+        [TestMethod]
+        public void HandlerNotFoundExceptionTest()
+        {
+            var container = new Container();
+            container.Register<IQueryDispatcher>(() => new QueryDispatcher(container));
+
+            var queryDispatcher = container.GetInstance<IQueryDispatcher>();
+
+            queryDispatcher.Dispatch<UserQuery, User>(new UserQuery());
+        }
+
+
+        class User { }
+        class UserQuery : IQuery { }
+    }
+}
